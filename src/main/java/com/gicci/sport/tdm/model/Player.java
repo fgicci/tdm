a1637fe05804dc7b42a16f12c98254cadff20d9e
Fernando Gicci Hernandes <fgicci@me.com>
2017-05-16 22:20:17 +0100

    development
    create constructor, getters and setters, hash and equal and toString to
    Player object.

--- src/main/java/com/gicci/sport/tdm/model/Player.java
+++ src/main/java/com/gicci/sport/tdm/model/Player.java
@@ -8,4 +8,106 @@ public class Player {
 	private String firstName;
 	private String lastName;
 	private Date birthDate;
+	private String email;
+	private String password;
+	private Boolean isActive;
+	
+	public Player() {}
+
+	public Player(Integer id, String firstName, String lastName, Date birthDate, String email, String password,  Boolean isActive) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthDate = birthDate;
+		this.email = email;
+		this.password = password;
+		this.isActive = isActive;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", birthDate=" + birthDate
+				+ ", email=" + email + ", password=" + password + ", isActive=" + isActive + "]";
+	}
 }
